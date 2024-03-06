7d0816fb438101dd46889ef6adf50787c70e2ed2
mansataqir <71388452+keizzy@users.noreply.github.com>
2024-03-06 13:10:58 +0000

    Update data Cleaning for Nashville Housing.sql

--- data Cleaning for Nashville Housing.sql
+++ data Cleaning for Nashville Housing.sql
@@ -11,17 +11,13 @@ FROM PortfolioProject..NashvilleHousing
 
 ALTER TABLE Nashvillehousing
 ADD Sale_date Date
-
+----------
 UPDATE NashvilleHousing
 SET sale_date = CONVERT(Date, SaleDate)
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
---------------------------------------------------POPULATING PROPERTY ADDRESS FOR NULL-------------------------------------------------------------------------------------- 
+--------------------------------------------------POPULATING PROPERTY ADDRESS WHERE THERE IS NULL---------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-SELECT * 
-FROM PortfolioProject..NashvilleHousing
-WHERE PropertyAddress IS NULL
 
 SELECT a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.propertyAddress, b.PropertyAddress)
 FROM NashvilleHousing a
@@ -29,7 +25,7 @@ JOIN NashvilleHousing b
   ON a.ParcelID = b.ParcelID
   AND a.[UniqueID ] <> b.[UniqueID ]
 WHERE a.PropertyAddress IS NULL
-
+---------
 UPDATE a
 SET PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
 FROM NashvilleHousing a
@@ -37,3 +33,74 @@ JOIN NashvilleHousing b
   ON a.ParcelID = b.ParcelID
   AND a.[UniqueID ] <> b.[UniqueID ]
 WHERE a.PropertyAddress IS NULL
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------BREAKING OUT PROPERTY ADDRESS INTO INDIVIDUAL COLUMNS(ADRESS, CITY, STATE)-------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+ALTER TABLE NashvilleHousing
+ADD PropertyAddressNew Nvarchar(255)
+------------
+UPDATE NashvilleHousing
+SET PropertyAddressNew =  SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) - 1)
+-------------
+ALTER TABLE NashvilleHousing
+ADD PropertyAddressNewCity Nvarchar(255)
+------------
+UPDATE NashvilleHousing
+SET PropertyAddressNewCity =  SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1, LEN(PropertyAddress))
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------BREAKING OUT OWNER ADDRESS INTO INDIVIDUAL COLUMNS(ADRESS, CITY, STATE)----------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- 
+
+ALTER TABLE NashvilleHousing
+ADD OwnerAddressNew Nvarchar(255)
+
+UPDATE NashvilleHousing
+SET OwnerAddressNew =  PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3)
+-------------
+ALTER TABLE NashvilleHousing
+ADD OwnerAddressNewCity Nvarchar(255)
+
+UPDATE NashvilleHousing
+SET OwnerAddressNewCity =  PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2)
+-------------
+ALTER TABLE NashvilleHousing
+ADD OwnerAddressNewState Nvarchar(255)
+
+UPDATE NashvilleHousing
+SET OwnerAddressNewState =  PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------CHANGING Y AND N TO YES AND NO IN "SOLD AS VACANT" FIELD---------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- 
+
+UPDATE NashvilleHousing
+SET SoldAsVacant = 
+  CASE
+	WHEN soldAsVacant = 'Y' THEN 'Yes'
+	WHEN soldAsVacant = 'N' THEN 'No'
+	ELSE SoldAsVacant
+  END
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------REMOVING DUPLICATES--------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- 
+
+WITH RowNumCTE AS 
+(
+SELECT *,
+  ROW_NUMBER() OVER (PARTITION BY ParcelID, PropertyAddress, SalePrice, Saledate, LegalReference ORDER BY UniqueID) Row_Num
+FROM NashvilleHousing
+)
+DELETE 
+FROM RowNumCTE
+WHERE Row_Num > 1
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------REMOVING UNUSED COLUMNS----------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- 
+
+ALTER TABLE NashvilleHousing
+DROP COLUMN  saledate, OwnerAddress, TaxDistrict, PropertyAddress
